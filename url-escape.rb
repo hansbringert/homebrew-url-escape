5a80f56b6c3e3647fb16c08075d92071d7591ccc
Hans Bringert <hans.bringert@infomaker.se>
2018-11-29 09:47:10 +0100

    Fixed formula

--- url-escape.rb
+++ url-escape.rb
@@ -1,15 +1,25 @@
+class url-escape < Formula
+  desc "url escape  CLI"
+  homepage "https://bitbucket.org/infomaker/"
   url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
-  sha256  "af8182851311c9b872f36ff53c7248272249f72e08852b7771dbd548f4faacd9"
+  sha256  "aab95b24d402a274f439e9294076c66b719d2bb52d47659a1d94b8411c972f01"
+
+  # depends_on "go" => :build
 
   def install
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
+	  bin.install "url-escape"
   end
 
   test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test oc-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
+    system "#{bin}/url-escape"
   end
 end
