87d56a3f287a30792e7d94d70bc1b82368ecf892
Hans Bringert <hans.bringert@infomaker.se>
2018-11-29 09:35:24 +0100

    version 0.1

--- url-escape.rb
+++ url-escape.rb
@@ -1,15 +1,15 @@
-class url-escape < Formula
-  desc "url escape command"
-  homepage "https://bitbucket.org/hansbringert/url-decode-encode"
   url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
-  sha256  ""
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
+  sha256  "af8182851311c9b872f36ff53c7248272249f72e08852b7771dbd548f4faacd9"
 
   def install
-	  bin.install "url-escape"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
   end
 
   test do
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/url-escape"
+  url  "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/url-escape/url-escape-0.1.tar.gz"
   end
 end
